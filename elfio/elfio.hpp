380e2dbfb1ddca3933461b1675cb39946e8e865c
Serge Lamikhov-Center <to_serge@users.sourceforge.net>
2012-11-25 00:23:05 +0200

    Remove tabs; Fix line endianess

--- elfio/elfio.hpp
+++ elfio/elfio.hpp
@@ -515,12 +515,12 @@ class elfio
         section* operator[]( unsigned int index ) const
         {
             section* sec = 0;
-			
+            
             if ( index < parent->sections_.size() ) {
-			    sec = parent->sections_[index];
-			}
-			
-			return sec;
+                sec = parent->sections_[index];
+            }
+            
+            return sec;
         }
 
 //------------------------------------------------------------------------------
