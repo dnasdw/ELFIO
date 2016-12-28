858ed4ceaa00b80df4ab896909ac45c6dc5d2964
Martin Bickel <martin.bickel@continental-corporation.com>
2016-12-28 18:19:17 +0100

    Check for errors when loading segments

--- elfio/elfio.hpp
+++ elfio/elfio.hpp
@@ -143,9 +143,8 @@ class elfio
         }
 
         load_sections( stream );
-        load_segments( stream );
-
-        return true;
+        bool is_still_good = load_segments( stream );
+        return is_still_good;
     }
 
 //------------------------------------------------------------------------------
