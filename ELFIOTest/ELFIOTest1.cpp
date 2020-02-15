f5d98c9a1456cdbe92ff7a877862c856ee6d6968
Serge Lamikhov-Center <to_serge@users.sourceforge.net>
2020-02-15 23:24:20 +0200

    Add autoxxx tools to the ELFIOTest directory

--- ELFIOTest/ELFIOTest1.cpp
+++ ELFIOTest/ELFIOTest1.cpp
@@ -5,7 +5,7 @@
 
 #define BOOST_TEST_NO_LIB
 #include <boost/test/unit_test.hpp>
-#include <boost/test/output_test_stream.hpp>
+#include <boost/test/tools/output_test_stream.hpp>
 using boost::test_tools::output_test_stream;
 
 #include <elfio/elfio.hpp>
