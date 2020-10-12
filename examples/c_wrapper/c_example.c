65b78de529b7ac6fc82129b8604ceaca888a8e0f
Serge Lamikhov-Center <to_serge@users.sourceforge.net>
2020-10-12 08:57:26 +0300

    Relocation C wrapper added

--- examples/c_wrapper/c_example.c
+++ examples/c_wrapper/c_example.c
@@ -19,7 +19,6 @@ int main( int argc, char* argv[] )
 
     /* Uncomment a block of the interest */
 
-    /*
     {
         int secno = elfio_get_sections_num( pelfio );
         printf( "\nSections No   : %d\n", secno );
@@ -30,16 +29,14 @@ int main( int argc, char* argv[] )
                 psection_t psection = elfio_get_section_by_index( pelfio, i );
                 char       buff[128];
                 elfio_section_get_name( psection, buff, 100 );
-                printf( "    [%02d] %s\n", i, buff );
-                printf( "        %08lx : %08lx\n",
-                        elfio_section_get_address( psection ),
-                        elfio_section_get_size( psection ) );
+                // printf( "    [%02d] %s\n", i, buff );
+                // printf( "        %08lx : %08lx\n",
+                //         elfio_section_get_address( psection ),
+                //         elfio_section_get_size( psection ) );
             }
         }
     }
-    */
 
-    /*
     {
         int segno = elfio_get_segments_num( pelfio );
         printf( "\nSegments No   : %d\n", segno );
@@ -48,16 +45,14 @@ int main( int argc, char* argv[] )
             int i;
             for ( i = 0; i < segno; i++ ) {
                 psegment_t psegment = elfio_get_segment_by_index( pelfio, i );
-                printf( "    [%02d] %08lx : %08lx : %08lx\n", i,
-                        elfio_segment_get_virtual_address( psegment ),
-                        elfio_segment_get_memory_size( psegment ),
-                        elfio_segment_get_file_size( psegment ) );
+                // printf( "    [%02d] %08lx : %08lx : %08lx\n", i,
+                //         elfio_segment_get_virtual_address( psegment ),
+                //         elfio_segment_get_memory_size( psegment ),
+                //         elfio_segment_get_file_size( psegment ) );
             }
         }
     }
-    */
 
-    /*
     {
         psection_t psection = elfio_get_section_by_name( pelfio, ".symtab" );
         psymbol_t  psymbols =
@@ -75,12 +70,11 @@ int main( int argc, char* argv[] )
                 unsigned char other;
                 elfio_symbol_get_symbol( psymbols, i, name, 128, &value, &size,
                                          &bind, &type, &section_index, &other );
-                printf( "[%4d] %10lu, %4lu %s\n", i, value, size, name );
+                // printf( "[%4d] %10lu, %4lu %s\n", i, value, size, name );
             }
         }
         elfio_symbol_section_accessor_delete( psymbols );
     }
-    */
 
     elfio_delete( pelfio );
 
