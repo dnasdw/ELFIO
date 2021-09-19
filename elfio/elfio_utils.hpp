e4cc3f99bdc4eb4f9147e67a7deb31ec6c5cdc48
Serge Lamikhov-Center <to_serge@hotmail.com>
2021-09-19 08:02:58 +0300

    An attempt to implement memory translation for sections

--- elfio/elfio_utils.hpp
+++ elfio/elfio_utils.hpp
@@ -193,7 +193,8 @@ class address_translator
             const address_translation& t = *it;
             if ( t.map_to <= value &&
                  ( ( value - t.map_to ) < ( t.end - t.start ) ) ) {
-                std::cout << std::hex << t.start - t.map_to + value << std::endl;
+                std::cout << std::hex << t.start - t.map_to + value << " "
+                          << value << std::endl;
                 return t.start - t.map_to + value;
             }
         }
