ef46e44e345ea7bd85d1cf62f36390df52641ee1
Serge Lamikhov-Center <to_serge@hotmail.com>
2022-06-19 17:34:28 +0300

    Prevent a memory leak

--- elfio/elfio.hpp
+++ elfio/elfio.hpp
@@ -615,6 +615,8 @@ class elfio
                                          static_cast<std::streampos>( i ) *
                                              entry_size ) ||
                  stream.fail() ) {
+                delete seg;
+                seg = nullptr;
                 return false;
             }
 
