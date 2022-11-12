9fc8ad129dc367afbb610425e564639fdb010e18
Serge Lamikhov-Center <to_serge@hotmail.com>
2022-11-12 17:00:08 +0200

    'noexept' added to upper level API members

--- elfio/elfio_section.hpp
+++ elfio/elfio_section.hpp
@@ -131,19 +131,19 @@ template <class T> class section_impl : public section
     ELFIO_GET_ACCESS( Elf64_Addr, address, header.sh_addr );
 
     //------------------------------------------------------------------------------
-    Elf_Half get_index() const override { return index; }
+    Elf_Half get_index() const noexcept override { return index; }
 
     //------------------------------------------------------------------------------
-    std::string get_name() const override { return name; }
+    std::string get_name() const noexcept override { return name; }
 
     //------------------------------------------------------------------------------
-    void set_name( const std::string& name_prm ) override
+    void set_name( const std::string& name_prm ) noexcept override
     {
         this->name = name_prm;
     }
 
     //------------------------------------------------------------------------------
-    void set_address( const Elf64_Addr& value ) override
+    void set_address( const Elf64_Addr& value ) noexcept override
     {
         header.sh_addr =
             typename section_header_impl_types<T>::Shdr_addr_type( value );
@@ -235,7 +235,7 @@ template <class T> class section_impl : public section
         typename section_header_impl_types<T>::Shdr_offset_type );
 
     //------------------------------------------------------------------------------
-    void set_index( const Elf_Half& value ) override { index = value; }
+    void set_index( const Elf_Half& value ) noexcept override { index = value; }
 
     //------------------------------------------------------------------------------
     bool load( std::istream& stream, std::streampos header_offset ) override
