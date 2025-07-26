507552193618c3f231a747087debd29f6534ddb7
Sun Daowen <sundaowen@gmail.com>
2025-07-27 01:32:31 +0800

    compat

--- tests/ELFIOTest1.cpp
+++ tests/ELFIOTest1.cpp
@@ -718,6 +718,33 @@ BOOST_AUTO_TEST_CASE(rearrange_local_symbols)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+struct relocation_symbol_callback : symbol_section_accessor::symbol_callback
+{
+    relocation_section_accessor& rela;
+
+    explicit relocation_symbol_callback(relocation_section_accessor& rela)
+        : rela(rela)
+    {
+    }
+
+    void operator()(Elf_Xword first, Elf_Xword second)
+    {
+        Elf64_Addr offset;
+        Elf_Word   symbol;
+        Elf_Word   rtype;
+        Elf_Sxword addend;
+        for (Elf_Word i = 0; i < rela.get_entries_num(); i++) {
+            rela.get_entry(i, offset, symbol, rtype, addend);
+            if (symbol == first) {
+                rela.set_entry(i, offset, second, rtype, addend);
+            }
+            if (symbol == second) {
+                rela.set_entry(i, offset, first, rtype, addend);
+            }
+        }
+    }
+};
+
 BOOST_AUTO_TEST_CASE(rearrange_local_symbols_with_reallocation)
 {
     std::string name              = "";
@@ -826,21 +853,7 @@ BOOST_AUTO_TEST_CASE(rearrange_local_symbols_with_reallocation)
         before.push_back(name);
     }
 
-    symbols.arrange_local_symbols([&](Elf_Xword first, Elf_Xword second) -> void {
-        Elf64_Addr offset;
-        Elf_Word   symbol;
-        Elf_Word   rtype;
-        Elf_Sxword addend;
-        for (Elf_Word i = 0; i < rela.get_entries_num(); i++) {
-            rela.get_entry(i,  offset, symbol, rtype, addend);
-            if (symbol == first) {
-                rela.set_entry(i, offset, second, rtype, addend);
-            }
-            if (symbol == second) {
-                rela.set_entry(i, offset, first, rtype, addend);
-            }
-        }
-    });
+    symbols.arrange_local_symbols(relocation_symbol_callback(rela));
 
     BOOST_REQUIRE_EQUAL(writer.save(file_name), true);
 
@@ -849,8 +862,8 @@ BOOST_AUTO_TEST_CASE(rearrange_local_symbols_with_reallocation)
     elfio reader;
     BOOST_REQUIRE_EQUAL(reader.load(file_name), true);
 
-    auto prelsec = reader.sections[".rel.text"];
-    auto psyms   = reader.sections[".symtab"];
+    section* prelsec = reader.sections[".rel.text"];
+    section* psyms   = reader.sections[".symtab"];
 
     BOOST_REQUIRE_NE(prelsec, nullptr);
     BOOST_REQUIRE_NE(psyms, nullptr);
