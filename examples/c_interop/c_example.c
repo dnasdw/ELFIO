cefbfa17ce2e2a0b854fbaa396ba5f7477fee03c
Sun Daowen <sundaowen@gmail.com>
2025-07-27 23:46:17 +0800

    compat

--- examples/c_interop/c_example.c
+++ examples/c_interop/c_example.c
@@ -17,28 +17,38 @@ int main( int argc, char* argv[] )
     printf( "Section Entry : %d\n", elfio_get_section_entry_size( pelfio ) );
     printf( "Segment Entry : %d\n", elfio_get_segment_entry_size( pelfio ) );
 
-    int secno = elfio_get_sections_num( pelfio );
-    printf( "\nSections No   : %d\n", secno );
+    {
+        int secno = elfio_get_sections_num( pelfio );
+        printf( "\nSections No   : %d\n", secno );
 
-    for ( int i = 0; i < secno; i++ ) {
-        psection_t psection = elfio_get_section_by_index( pelfio, i );
-        char       buff[128];
-        elfio_section_get_name( psection, buff, 100 );
-        printf( "    [%02d] %s\n", i, buff );
-        printf( "        %08lx : %08lx\n",
-                elfio_section_get_address( psection ),
-                elfio_section_get_size( psection ) );
+        {
+            int i;
+            for ( i = 0; i < secno; i++ ) {
+                psection_t psection = elfio_get_section_by_index( pelfio, i );
+                char       buff[128];
+                elfio_section_get_name( psection, buff, 100 );
+                printf( "    [%02d] %s\n", i, buff );
+                printf( "        %08lx : %08lx\n",
+                        elfio_section_get_address( psection ),
+                        elfio_section_get_size( psection ) );
+            }
+        }
     }
 
-    int segno = elfio_get_segments_num( pelfio );
-    printf( "\nSegments No   : %d\n", segno );
+    {
+        int segno = elfio_get_segments_num( pelfio );
+        printf( "\nSegments No   : %d\n", segno );
 
-    for ( int i = 0; i < segno; i++ ) {
-        psegment_t psegment = elfio_get_segment_by_index( pelfio, i );
-        printf( "    [%02d] %08lx : %08lx : %08lx\n", i,
-                elfio_segment_get_virtual_address( psegment ),
-                elfio_segment_get_memory_size( psegment ),
-                elfio_segment_get_file_size( psegment ) );
+        {
+            int i;
+            for ( i = 0; i < segno; i++ ) {
+                psegment_t psegment = elfio_get_segment_by_index( pelfio, i );
+                printf( "    [%02d] %08lx : %08lx : %08lx\n", i,
+                        elfio_segment_get_virtual_address( psegment ),
+                        elfio_segment_get_memory_size( psegment ),
+                        elfio_segment_get_file_size( psegment ) );
+            }
+        }
     }
 
     elfio_delete( pelfio );
